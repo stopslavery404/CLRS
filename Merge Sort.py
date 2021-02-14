61bf69c965a1e62f7d941eca89d696bd05f3cfa8
RAHUL KUMAR <55033230+pctablet505@users.noreply.github.com>
2021-02-15 03:46:00 +0530

    Update Merge Sort.py

--- Merge Sort.py
+++ Merge Sort.py
@@ -1,3 +1,45 @@
+def merge_sort(arr, low, high):
+    """Program for insertion sort
+        Inplace sorting
+        Space Complexity O(n)   a temporary array of linear size is used
+        for each sub problem in merge setp
+        Time complexity O(n log (n))
+        >>> arr = [1, 5, 0, -3, 5, 7, 2, 0, 9, 1]
+        >>> print(arr)
+        [1, 5, 0, -3, 5, 7, 2, 0, 9, 1]
+        >>> low=0
+        >>> high = len(arr) - 1
+        >>> merge_sort(arr, low, high)
+        >>> print(arr)
+        [-3, 0, 0, 1, 1, 2, 5, 5, 7, 9]
+        """
+
+    if low < high:
+        mid = (low + high) // 2
+
+        merge_sort(arr, low, mid)  # Recursively sort first half
+        merge_sort(arr, mid + 1, high)  # Recursively sort second half
+        merge(arr, low, mid, high)  # merge the two already sorted subarrays
+
+
+def merge(arr, low, mid, high):
+    L = arr[low:mid + 1]
+    R = arr[mid + 1:high + 1]
+    L.append(float('inf'))
+    R.append(float('inf'))
+    i = j = 0
+    for k in range(low, high + 1):
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+
+
+'''    
+# Simpler implementation for python
+
 def merge_sort(array):
     """Program for insertion sort
     Inplace sorting
@@ -13,7 +55,7 @@ def merge_sort(array):
     """
 
     def merge_sort_util(arr):
-        if len(arr)>1:
+        if len(arr) > 1:
             mid = len(arr) // 2
             L = merge_sort_util(arr[:mid])
             R = merge_sort_util(arr[mid:])
@@ -35,11 +77,10 @@ def merge_sort(array):
         result.extend(arr2[j:])
         return result
 
-    arr[:]=merge_sort_util(array)
+    arr[:] = merge_sort_util(array)
 
-
+'''
 arr = [1, 5, 0, -3, 5, 7, 2, 0, 9, 1]
 print(arr)
-merge_sort(arr)
+merge_sort(arr, 0, len(arr) - 1)
 print(arr)
-merge_sort(arr)
